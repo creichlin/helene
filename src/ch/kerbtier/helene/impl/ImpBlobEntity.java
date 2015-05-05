0d3f5ade40d4a593c8132385aa194f1a8be562ee
Christian Reichlin <chrigi@kerbtier.ch>
2015-05-05 23:04:30 +0200

    fixed blob bug, entity extending from wrong interface

--- src/ch/kerbtier/helene/impl/ImpBlobEntity.java
+++ src/ch/kerbtier/helene/impl/ImpBlobEntity.java
@@ -1,10 +1,10 @@
 package ch.kerbtier.helene.impl;
 
 import ch.kerbtier.helene.HBlob;
-import ch.kerbtier.helene.entities.DateEntity;
+import ch.kerbtier.helene.entities.BlobEntity;
 import ch.kerbtier.helene.entities.Entity;
 
-public class ImpBlobEntity extends ImpEntity implements DateEntity {
+public class ImpBlobEntity extends ImpEntity implements BlobEntity {
 
   public ImpBlobEntity(Entity parent, String name) {
     super(parent, name);
